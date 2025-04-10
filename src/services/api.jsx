dab63f6e6bdee6031789d160969f904dc63e92f8
Elavarasan2023IT014 <elavarasan.r2023it@sece.ac.in>
2025-04-10 18:52:07 +0530

    SAVE

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,25 +1,13 @@
-// src/api.js
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://demo-checkin-backend.onrender.com/api',
-  withCredentials: true, // Optional if needed
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('employeeToken');
-  if (token) {
-    config.headers['x-employee-token'] = token;
-  }
+  if (token) config.headers['x-employee-token'] = token;
   return config;
-}, (error) => {
-  console.error('Request error:', error);
-  return Promise.reject(error);
-});
-
-api.interceptors.response.use((response) => response, (error) => {
-  console.error('Response error:', error.response ? error.response.data : error.message);
-  return Promise.reject(error);
 });
 
 export default api;
