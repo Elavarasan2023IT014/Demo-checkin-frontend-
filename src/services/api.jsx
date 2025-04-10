b3e120caf28466674de7068402b7eb98ccd5c270
Elavarasan2023IT014 <elavarasan.r2023it@sece.ac.in>
2025-04-10 17:31:18 +0530

    SAVE

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -7,7 +7,16 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('employeeToken');
   if (token) config.headers['x-employee-token'] = token;
+  console.log('Request URL:', config.url);
   return config;
+}, (error) => {
+  console.error('Request error:', error);
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => response, (error) => {
+  console.error('Response error:', error.response ? error.response.data : error.message);
+  return Promise.reject(error);
 });
 
 export default api;
