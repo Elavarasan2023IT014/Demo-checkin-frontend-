dcdb8f49e90559cf8f61c09eaf0736ef0e23a6e8
Elavarasan2023IT014 <elavarasan.r2023it@sece.ac.in>
2025-04-10 16:13:20 +0530

    SAVE

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,13 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://demo-checkin-backend.onrender.com', // Updated to deployed backend URL
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('employeeToken');
   if (token) config.headers['x-employee-token'] = token;
+  console.log('Request URL:', config.url); // Debug log for request URL
   return config;
+}, (error) => {
+  console.error('Request error:', error);
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => response, (error) => {
+  console.error('Response error:', error.response ? error.response.data : error.message);
+  return Promise.reject(error);
 });
 
 export default api;
